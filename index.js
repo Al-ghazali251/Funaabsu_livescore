58e48c2a81bed4f01caeefa17ad9811e269eb087
Dhikrullah <dhikrullahkehinde@gmail.com>
2025-09-10 09:41:41 +0100

    Fetch fixtures

--- index.js
+++ index.js
@@ -572,6 +572,28 @@ app.post("/players/bulk", async (req, res) => {
   }
 });
 
+const express = require("express");
+const router = express.Router();
+const Fixture = require("../model/Fixture");
+
+// GET: Get all fixtures
+app.get("/fetch-fixtures", async (req, res) => {
+  try {
+    const fixtures = await Fixture.find();
+
+    if (!fixtures || fixtures.length === 0) {
+      return res.status(404).json({ message: "No fixtures found" });
+    }
+
+    res.status(200).json(fixtures);
+  } catch (error) {
+    console.error("Error fetching fixtures:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+
+
   app.listen(3000, '0.0.0.0', () => {
     console.log("Server is running on port 3000");
   });
